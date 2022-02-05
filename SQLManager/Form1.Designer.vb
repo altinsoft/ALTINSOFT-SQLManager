61fa9fd2478604ef7eb23c1ab86096cbd201ae33
AltinSoft Information Technologies <mehmettalas@gmail.com>
2022-02-05 16:20:45 +0300

    create

---
+++ SQLManager/Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DatabaseList = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SqlHostName = New System.Windows.Forms.ComboBox()
+        Me.SqlInstance = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Sonuc = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Tablolar = New System.Windows.Forms.ComboBox()
+        Me.Tablolar1 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.scope = New System.Windows.Forms.CheckBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label1.Location = New System.Drawing.Point(26, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Veri Tabanları"
+        '
+        'DatabaseList
+        '
+        Me.DatabaseList.BackColor = System.Drawing.Color.White
+        Me.DatabaseList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.DatabaseList.FormattingEnabled = True
+        Me.DatabaseList.Location = New System.Drawing.Point(29, 93)
+        Me.DatabaseList.Name = "DatabaseList"
+        Me.DatabaseList.Size = New System.Drawing.Size(216, 21)
+        Me.DatabaseList.TabIndex = 101
+        Me.DatabaseList.Tag = ""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label2.Location = New System.Drawing.Point(26, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 13)
+        Me.Label2.TabIndex = 102
+        Me.Label2.Text = "HostName"
+        '
+        'SqlHostName
+        '
+        Me.SqlHostName.BackColor = System.Drawing.Color.White
+        Me.SqlHostName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.SqlHostName.FormattingEnabled = True
+        Me.SqlHostName.Items.AddRange(New Object() {"(localdb)", "localhost"})
+        Me.SqlHostName.Location = New System.Drawing.Point(29, 41)
+        Me.SqlHostName.Name = "SqlHostName"
+        Me.SqlHostName.Size = New System.Drawing.Size(216, 21)
+        Me.SqlHostName.TabIndex = 103
+        Me.SqlHostName.Tag = ""
+        '
+        'SqlInstance
+        '
+        Me.SqlInstance.Location = New System.Drawing.Point(251, 42)
+        Me.SqlInstance.Name = "SqlInstance"
+        Me.SqlInstance.Size = New System.Drawing.Size(150, 20)
+        Me.SqlInstance.TabIndex = 104
+        Me.SqlInstance.Text = "ALTINSOFT"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label3.Location = New System.Drawing.Point(248, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 13)
+        Me.Label3.TabIndex = 105
+        Me.Label3.Text = "Instance"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(407, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 27)
+        Me.Button1.TabIndex = 106
+        Me.Button1.Text = "Bağlan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Sonuc
+        '
+        Me.Sonuc.Location = New System.Drawing.Point(29, 144)
+        Me.Sonuc.Multiline = True
+        Me.Sonuc.Name = "Sonuc"
+        Me.Sonuc.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Sonuc.Size = New System.Drawing.Size(740, 173)
+        Me.Sonuc.TabIndex = 107
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label4.Location = New System.Drawing.Point(26, 128)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 13)
+        Me.Label4.TabIndex = 108
+        Me.Label4.Text = "SQL Cümlesi"
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button2.Location = New System.Drawing.Point(497, 333)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(133, 50)
+        Me.Button2.TabIndex = 109
+        Me.Button2.Text = "Insert Oluştur"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button3.Location = New System.Drawing.Point(636, 333)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(133, 50)
+        Me.Button3.TabIndex = 110
+        Me.Button3.Text = "Update Oluştur"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Tablolar
+        '
+        Me.Tablolar.BackColor = System.Drawing.Color.White
+        Me.Tablolar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Tablolar.FormattingEnabled = True
+        Me.Tablolar.Location = New System.Drawing.Point(251, 93)
+        Me.Tablolar.Name = "Tablolar"
+        Me.Tablolar.Size = New System.Drawing.Size(216, 21)
+        Me.Tablolar.TabIndex = 112
+        Me.Tablolar.Tag = ""
+        '
+        'Tablolar1
+        '
+        Me.Tablolar1.AutoSize = True
+        Me.Tablolar1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Tablolar1.Location = New System.Drawing.Point(248, 77)
+        Me.Tablolar1.Name = "Tablolar1"
+        Me.Tablolar1.Size = New System.Drawing.Size(53, 13)
+        Me.Tablolar1.TabIndex = 111
+        Me.Tablolar1.Text = "Tablolar"
+        '
+        'Button4
+        '
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button4.Location = New System.Drawing.Point(29, 333)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(101, 50)
+        Me.Button4.TabIndex = 113
+        Me.Button4.Text = "Hafızaya AL"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'scope
+        '
+        Me.scope.AutoSize = True
+        Me.scope.Location = New System.Drawing.Point(358, 351)
+        Me.scope.Name = "scope"
+        Me.scope.Size = New System.Drawing.Size(124, 17)
+        Me.scope.TabIndex = 114
+        Me.scope.Text = "SCOPE_IDENTITY()"
+        Me.scope.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button5.Location = New System.Drawing.Point(136, 333)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(183, 50)
+        Me.Button5.TabIndex = 115
+        Me.Button5.Text = "Parametreleri Oluştur"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 414)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.scope)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Tablolar)
+        Me.Controls.Add(Me.Tablolar1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Sonuc)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.SqlInstance)
+        Me.Controls.Add(Me.SqlHostName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DatabaseList)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AltınSoft SQL Manager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DatabaseList As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents SqlHostName As ComboBox
+    Friend WithEvents SqlInstance As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Sonuc As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Tablolar As ComboBox
+    Friend WithEvents Tablolar1 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents scope As CheckBox
+    Friend WithEvents Button5 As Button
+End Class
---
+++ SQLManager/Form1.vb
@@ -0,0 +1,198 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Function MasterConnection() As SqlConnection
+        Try
+            If SqlHostName.Text = "localhost" Then
+                If SqlInstance.Text = "" Then
+                    Return New SqlConnection("Data Source=localhost;Initial Catalog=master;Integrated Security=True")
+                Else
+                    Return New SqlConnection("Data Source=localhost\" + SqlInstance.Text + ";Initial Catalog=master;Integrated Security=True")
+                End If
+            Else
+                Return New SqlConnection("Data Source=(localdb)\" + SqlInstance.Text + ";Initial Catalog=master;Integrated Security=True")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+            Dim a As New SqlConnection
+            Return a
+        End Try
+    End Function
+    Function Connection() As SqlConnection
+        Try
+            If SqlHostName.Text = "localhost" Then
+                If SqlInstance.Text = "" Then
+                    Return New SqlConnection("Data Source=localhost;Initial Catalog=" + DatabaseList.Text + ";Integrated Security=True")
+                Else
+                    Return New SqlConnection("Data Source=localhost\" + SqlInstance.Text + ";Initial Catalog=" + DatabaseList.Text + ";Integrated Security=True")
+                End If
+            Else
+                Return New SqlConnection("Data Source=(localdb)\" + SqlInstance.Text + ";Initial Catalog=" + DatabaseList.Text + ";Integrated Security=True")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+            Dim a As New SqlConnection
+            Return a
+        End Try
+    End Function
+    Function GetTable(Connection_ As SqlConnection, cmd As SqlCommand) As DataTable
+        Try
+            Dim _da = New SqlDataAdapter()
+            Dim table = New DataTable("DATA")
+            Dim _conn = Connection_
+            Try
+                _conn.Open()
+                cmd.Connection = _conn
+                _da.SelectCommand = cmd
+                _da.SelectCommand.CommandTimeout = 0
+                table.Locale = Globalization.CultureInfo.InvariantCulture
+                _da.Fill(table)
+                _da.Dispose()
+            Catch ex As Exception
+                MessageBox.Show(ex.ToString)
+            Finally
+                _conn.Close()
+                _da.Dispose()
+                _conn.Dispose()
+            End Try
+            If table IsNot Nothing Then
+                If table.Rows.Count > 0 Then
+                    Return table
+                Else
+                    Return Nothing
+                End If
+            Else
+                Return Nothing
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+            Return Nothing
+        End Try
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim conn = MasterConnection()
+
+            DatabaseList.BeginInvoke(New Action(Sub()
+
+                                                    DatabaseList.DisplayMember = "Text"
+                                                    DatabaseList.ValueMember = "Value"
+                                                    Dim tb As New DataTable
+                                                    tb.Columns.Add("Text", GetType(String))
+                                                    tb.Columns.Add("Value", GetType(Long))
+                                                    Dim cmd As New SqlCommand("SELECT name, database_id FROM sys.databases")
+                                                    Dim tablo As DataTable = GetTable(conn, cmd)
+                                                    If tablo IsNot Nothing Then
+                                                        For i = 0 To tablo.Rows.Count - 1
+                                                            If tablo.Rows(i)("name").ToString <> "master" And tablo.Rows(i)("name").ToString <> "tempdb" And tablo.Rows(i)("name").ToString <> "model" And tablo.Rows(i)("name").ToString <> "msdb" Then
+                                                                tb.Rows.Add(tablo.Rows(i)("name").ToString, tablo.Rows(i)("database_id"))
+                                                            End If
+                                                        Next
+                                                        DatabaseList.DataSource = tb
+                                                    End If
+                                                End Sub))
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+    End Sub
+
+
+    Private Sub DatabaseList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DatabaseList.SelectedIndexChanged
+        Try
+            Tablolar.Items.Clear()
+            Dim conn = Connection()
+            Dim cmd As New SqlCommand("SELECT TABLE_NAME as Tablolar FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='" + DatabaseList.Text + "' order by TABLE_NAME")
+            Dim tablo As DataTable = GetTable(conn, cmd)
+            If tablo IsNot Nothing Then
+                For i = 0 To tablo.Rows.Count - 1
+                    If tablo.Rows(i)("Tablolar").ToString <> "sysdiagrams" Then
+                        Tablolar.Items.Add(tablo.Rows(i)("Tablolar").ToString)
+                    End If
+                Next
+            Else
+                MessageBox.Show("Tablo bilgisi alınamadı !")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim a1 As String = Nothing
+            Dim a2 As String = Nothing
+            Dim conn = Connection()
+            Dim cmd As New SqlCommand("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + Tablolar.Text + "'")
+            Dim tablo As DataTable = GetTable(conn, cmd)
+            If tablo IsNot Nothing Then
+                For i = 0 To tablo.Rows.Count - 1
+                    a1 += tablo.Rows(i)("COLUMN_NAME").ToString + ","
+                    a2 += "@" + tablo.Rows(i)("COLUMN_NAME").ToString + ","
+                Next
+                a1 = a1 + ","
+                a1 = a1.Replace(",,", "")
+                a2 = a2 + ","
+                a2 = a2.Replace(",,", "")
+                If scope.Checked Then
+                    Sonuc.Text = "insert into " + Tablolar.Text + " (" + a1 + ") values(" + a2 + ") SELECT SCOPE_IDENTITY()"
+                Else
+                    Sonuc.Text = "insert into " + Tablolar.Text + " (" + a1 + ") values(" + a2 + ")"
+                End If
+            Else
+                MessageBox.Show("Tablo kolon bilgisi alınamadı !")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            Dim a1 As String = Nothing
+            Dim conn = Connection()
+            Dim cmd As New SqlCommand("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + Tablolar.Text + "'")
+            Dim tablo As DataTable = GetTable(conn, cmd)
+            If tablo IsNot Nothing Then
+                For i = 0 To tablo.Rows.Count - 1
+                    a1 += tablo.Rows(i)("COLUMN_NAME").ToString + "=@" + tablo.Rows(i)("COLUMN_NAME").ToString + ","
+                Next
+                a1 = a1 + ","
+                a1 = a1.Replace(",,", "")
+                Sonuc.Text = "update " + Tablolar.Text + " set " + a1
+            Else
+                MessageBox.Show("Tablo kolon bilgisi alınamadı !")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Clipboard.Clear()
+        Clipboard.SetText(Sonuc.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SqlHostName.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Try
+            Dim a1 As String = Nothing
+            Dim conn = Connection()
+            Dim cmd As New SqlCommand("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + Tablolar.Text + "'")
+            Dim tablo As DataTable = GetTable(conn, cmd)
+            If tablo IsNot Nothing Then
+                For i = 0 To tablo.Rows.Count - 1
+                    a1 += "cmd.Parameters.AddWithValue(""@" + tablo.Rows(i)("COLUMN_NAME").ToString + """," + tablo.Rows(i)("COLUMN_NAME").ToString + ")" + Environment.NewLine
+                Next
+                Sonuc.Text = a1
+            Else
+                MessageBox.Show("Tablo kolon bilgisi alınamadı !")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+    End Sub
+End Class
